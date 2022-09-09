cbcc87622c0e300da699793a0a7dcb8c40827fe7
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2022-09-09 14:39:04 -0700

    fix displayMessageActivity

--- app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.kt
@@ -2,12 +2,17 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTitle("My Message")
+
         setContentView(R.layout.activity_display_message_activity)
 
         val message: String? = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+        val textView: TextView = findViewById(R.id.my_textview)
+        textView.text = message
     }
 }
